6d4f9e6b380cc374edaee80830de8bbb1f92c8de
Pedro Rojas <pedro.rojasm3@userena.cl>
2025-09-12 10:45:19 -0300

    Actualizacion visual de /home acorde al wireframe discutido

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,29 @@
+//import "./Home.css";
+import "../styles/Global.css"
+
 export default function Home() {
-    return (
-      <div style={{ padding: "2rem" }}>
-        <h1>Pedro</h1>
-        <img src="https://i.scdn.co/image/ab67616d0000b273434b3102f10c5c2a4ab68392" alt="foto" />
-      </div>
-    );
-  }
+  return (
+    <div className="screen-container">
+      <header className="screen-header">
+        <img 
+          src="/logo.png" 
+          alt="SmartParking Logo" 
+          className="logo"
+        />
+      </header>
+
+      <main className="home-main">
+        <h1>Menú</h1>
+        <p className="subtitle">Selecciona la opción:</p>
+
+        <div className="menu-buttons">
+          <button className="menu-btn">Registro Ingreso/Salida</button>
+          <button className="menu-btn">Reportes</button>
+          <button className="menu-btn">Panel de Control</button>
+          <button className="menu-btn">Contacto Soporte</button>
+          <button className="menu-btn">Ajustes</button>
+        </div>
+      </main>
+    </div>
+  );
+}
